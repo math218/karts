7e7b1af9933706be18ff445de21b02b07b16618f
math218 <iker.marquez@colegiolakeside.edu.mx>
2025-01-10 10:19:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -40,30 +40,15 @@ boton.addEventListener('click', () => {
       alert("Vuelve en 5 días o contacta al administrador");
     } else {
       const contrasena = contrasenaInput.value;
-      if (contrasena === "algo_dificil") {
-        window.location.href = "index.html?mensaje=sigma_boy";
-      } else {
-        intentos--;
-        manejarCookies.setCookie("intentos", intentos, 5);
-        alert(`Te quedan ${intentos} intentos`);
-      }
+
     }
   }
 });
 
 // Agregar eventos para combinaciones de teclado
 addEventListener("keydown", async function (event) {
-  if (event.shiftKey && event.ctrlKey) {
-    manejarCookies.eraseCookie("intentos");
-    intentos = 3; // Reiniciar la variable también
-    alert("Se han eliminado los intentos");
-  } else if (event.altKey && event.shiftKey) {
-    try {
-      await navigator.clipboard.writeText("もうせ");
-      alert("Texto copiado al portapapeles");
-    } catch (error) {
-      console.error("Error al copiar al portapapeles:", error);
-      alert("No se pudo copiar el texto. Verifica los permisos.");
-    }
+  if (event.key == "enter" && event.key == "1") {
+    window.location.href = "index.html?mensaje=nosigma";
   }
+  
 });
