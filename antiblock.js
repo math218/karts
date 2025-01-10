c57f0175ed05ea409d286f4a4fc4fb92a49b79c4
math218 <iker.marquez@colegiolakeside.edu.mx>
2025-01-10 10:23:54 -0600

    Update antiblock.js

--- antiblock.js
+++ antiblock.js
@@ -1,5 +1,3 @@
         const params = new URLSearchParams(window.location.search);
         const mensaje = params.get("mensaje");
-        if (mensaje != "nosigma") {
-            window.location.href= "index2.html";
-        }
+=
