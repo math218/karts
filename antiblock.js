cbc99c80bac3df7cb1824637a86deb41c7fd47f1
math218 <iker.marquez@colegiolakeside.edu.mx>
2025-01-10 18:06:30 -0600

    Update antiblock.js

--- antiblock.js
+++ antiblock.js
@@ -1,3 +1,9 @@
-        const params = new URLSearchParams(window.location.search);
-        const mensaje = params.get("mensaje");
-=
+    // Leer datos del formulario POST
+    const params = new URLSearchParams(window.location.search);
+    const email = params.get('email');
+
+    // Verificar si el correo está presente
+    if (!email) {
+      alert("Unauthorized access. Redirecting to login.");
+      window.location.href = "login.html";
+    }
