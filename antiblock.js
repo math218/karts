27e4c2cce4a002abf0117db5546814c4ae214e3a
math218 <iker.marquez@colegiolakeside.edu.mx>
2025-01-12 21:18:18 -0600

    fsds

--- antiblock.js
+++ antiblock.js
@@ -4,15 +4,10 @@
     const admin_enter = params.get('admin_enter');
 
 
-    function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-  return null;
-}
+
 var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
     // Verificar si el correo está presente
-    if (!email && getCookie("userEmail") == null && admin_enter != "belu-mom" && validEmail.test(email) == false) {
+    if (!email) {
       alert("Unauthorized access. Redirecting to login.");
       window.location.href = "login.html";
     }
