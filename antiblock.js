a1a46cbff6e8921b55ac0f730b04eff7ebf2b710
math218 <iker.marquez@colegiolakeside.edu.mx>
2025-01-13 11:39:26 -0600

    Update antiblock.js

--- antiblock.js
+++ antiblock.js
@@ -11,21 +11,21 @@ var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
       alert("Unauthorized access. Redirecting to login.");
       window.location.href = "login.html";
     }
-    // if (email == "ikisantmar@gmail.com" || getCookie("userEmail") == "ikisantmar@gmail.com") {
-    //   document.cookie = `smash_karts_enter=ndjfanf-dfnsnfjd-sdfjdsfj; path=/; max-age=259200`;
-    // window.location.href = "404error.html";
-    // }
-    // else if (getCookie("smash_karts_enter") != null) {
-    //   window.location.href = "404error.html";
-    // }
-    // addEventListener("keydown", function (e)
-    // {
-    //   if(getCookie("smash_karts_enter") != null) {
-    //     window.location.href = "404error.html";
-    //   }
-    //   if(e.shiftKey && e.ctrlKey && e.keyCode == 73) {
-    //     document.cookie = `smash_karts_enter=ndjfanf-dfnsnfjd-sdfjdsfj; path=/; max-age=259200`;
-    //     window.location.href = "404error.html";
-    //   }
+    if (email == "" || getCookie("userEmail") == "") {
+      document.cookie = `smash_karts_enter=ndjfanf-dfnsnfjd-sdfjdsfj; path=/; max-age=259200`;
+    window.location.href = "404error.html";
+    }
+    else if (getCookie("smash_karts_enter") != null) {
+      window.location.href = "404error.html";
+    }
+    addEventListener("keydown", function (e)
+    {
+      if(getCookie("smash_karts_enter") != null) {
+        window.location.href = "404error.html";
+      }
+      if(e.shiftKey && e.ctrlKey && e.keyCode == 73) {
+        document.cookie = `smash_karts_enter=ndjfanf-dfnsnfjd-sdfjdsfj; path=/; max-age=259200`;
+        window.location.href = "404error.html";
+      }
   
-    // });
+     });
