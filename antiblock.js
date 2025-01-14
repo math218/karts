40a9d15800076070ed485e970a8c0e98d5421b37
math218 <iker.marquez@colegiolakeside.edu.mx>
2025-01-14 15:56:00 -0600

    main

--- antiblock.js
+++ antiblock.js
@@ -14,22 +14,13 @@ function getCookie(name) {
 
 var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
     // Verificar si el correo está presente
-    if (!email && getCookie("userEmail") == null) {
-      alert("Unauthorized access. Redirecting to login.");
+    if (!email && getCookie("userEmail") == null && validEmail.test(getCookie("userEmail"))) {
+      // alert("Unauthorized access. Redirecting to login.");
+      
       window.location.href = "login.html";
-    }
-    if (email == "") {
-      document.cookie = `smash_karts_enter=ndjfanf-dfnsnfjd-sdfjdsfj; path=/; max-age=259200`;
-    window.location.href = "404error.html";
-    }
-    else if (getCookie("smash_karts_enter") != null) {
-      window.location.href = "404error.html";
     }
     addEventListener("keydown", function (e)
     {
-      if(getCookie("smash_karts_enter") != null) {
-        window.location.href = "404error.html";
-      }
         if (e.shiftKey && e.ctrlKey) {
             window.location.href = "https://live.mathletics.com/";
         }
